c52c110579ca1f1167f6ae7a7bbce2070fd341e0
Pasindu-Madhuwantha <mpasindu72@gmail.com>
2026-02-04 08:22:21 +0530

    Deploy Test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -19,6 +19,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3002);
+  const port = process.env.PORT || 8080;
+  await app.listen(port);
+  console.log(`Application is running on port ${port}`);
 }
 bootstrap();
